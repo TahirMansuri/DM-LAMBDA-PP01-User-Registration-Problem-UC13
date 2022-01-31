5f643165a50a2b77fa80696b3d2a58452faff2d3
TahirMansuri <tprocks786@gmail.com>
2022-01-31 21:25:51 +0530

    ADDED: Modified User Registration using LAMBDA Expression

---
+++ README.md
@@ -0,0 +1,1 @@
+# Welcome to DM LAMBDA PP01 User Registration Problem UC13
---
+++ src/com/bridgelabz/UserRegistrationRegex.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class UserRegistrationRegex {
+    public static void main(String[] args) {
+        UserRegistration userInformation = new UserRegistration();
+
+        System.out.println(userInformation.userFirstName());
+        System.out.println(userInformation.userLastName());
+        System.out.println(userInformation.userEmailId());
+        System.out.println(userInformation.userMobNum());
+        System.out.println(userInformation.userPassword());
+    }
+
+}
